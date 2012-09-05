b92002c50b06bc240b8507a855ad67cf5bf2cce9
lajos papp <lalyos@yahoo.com>
2012-09-05 16:33:41 +0200

    beanFactory -> ApplicationContext

--- src/main/java/com/github/lalyos/kore/App.java
+++ src/main/java/com/github/lalyos/kore/App.java
@@ -1,10 +1,7 @@
 package com.github.lalyos.kore;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.FileSystemResource;
 
 /**
  * Hello world!
@@ -14,7 +11,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        //BeanFactory beanFactory = new XmlBeanFactory(new FileSystemResource("src/main/resources/META-INF/spring/beans.xml"));
         ApplicationContext ctx = new ClassPathXmlApplicationContext("META-INF/spring/beans.xml");
         GreetingApp app = ctx.getBean(GreetingApp.class);
         app.greetEverybody();
