9b8091d89abf85fb443bf6faa861cb37ea0e729d
lajos papp <lalyos@yahoo.com>
2012-09-05 16:26:24 +0200

    constructor style DI added

--- src/main/java/com/github/lalyos/kore/SimpleGreetingService.java
+++ src/main/java/com/github/lalyos/kore/SimpleGreetingService.java
@@ -2,7 +2,14 @@ package com.github.lalyos.kore;
 
 public class SimpleGreetingService implements GreetingService {
 
+    private String greeting;
+
+    public SimpleGreetingService(String greeting) {
+        super();
+        this.greeting = greeting;
+    }
+
     public void greet(String msg) {
-        System.out.println("hello " + msg);
+        System.out.println(greeting + " " + msg);
     }
 }
