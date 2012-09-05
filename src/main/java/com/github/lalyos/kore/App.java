eecf30dd4e4dfd6b45e424bdc730c37687ab0461
lajos papp <lalyos@yahoo.com>
2012-09-05 16:33:17 +0200

    switching from BeanFactory to ApplicationContext

--- src/main/java/com/github/lalyos/kore/App.java
+++ src/main/java/com/github/lalyos/kore/App.java
@@ -2,6 +2,8 @@ package com.github.lalyos.kore;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 /**
@@ -12,8 +14,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        BeanFactory beanFactory = new XmlBeanFactory(new FileSystemResource("src/main/resources/META-INF/spring/beans.xml"));
-        GreetingApp app = beanFactory.getBean(GreetingApp.class);
+        //BeanFactory beanFactory = new XmlBeanFactory(new FileSystemResource("src/main/resources/META-INF/spring/beans.xml"));
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("META-INF/spring/beans.xml");
+        GreetingApp app = ctx.getBean(GreetingApp.class);
         app.greetEverybody();
     }
 }
