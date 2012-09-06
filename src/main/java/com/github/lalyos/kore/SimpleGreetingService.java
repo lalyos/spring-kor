eb63ef1a3c67335de31395fa6951ad317a4fd176
lajos papp <lalyos@yahoo.com>
2012-09-06 13:45:52 +0200

    autowire byType fails "expected single matching bean but found 2"

--- src/main/java/com/github/lalyos/kore/SimpleGreetingService.java
+++ src/main/java/com/github/lalyos/kore/SimpleGreetingService.java
@@ -1,10 +1,16 @@
 package com.github.lalyos.kore;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class SimpleGreetingService implements GreetingService {
 
     private String greeting;
 
-    public SimpleGreetingService(String greeting) {
+    @Autowired
+    public SimpleGreetingService(@Value("Hello ")String greeting) {
         super();
         this.greeting = greeting;
     }
